b53d20d379bca675144f1b0f705d6110bd881337
Jaydeep2020 <110714883+Jaydeep2020@users.noreply.github.com>
2025-06-19 10:08:12 +0530

    Update Multiply_NumString.py

--- Multiply_NumString.py
+++ Multiply_NumString.py
@@ -8,9 +8,9 @@ def MultiplyStr(num1, num2):
         n = n + (num1%10) * base
         base = base * 10
         num1 = int(num1/10)
-        
-    base = 1
+
     m = 0
+    base = 1
     
     while num2>0:
         m = m + (num2%10) * base
