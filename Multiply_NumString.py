e4b31655fb4fcb7989b39df83e65eca4a76bedd2
Jaydeep2020 <110714883+Jaydeep2020@users.noreply.github.com>
2025-06-19 10:06:12 +0530

    Create Multiply_NumString.py

---
+++ Multiply_NumString.py
@@ -0,0 +1,28 @@
+# Multiply Num String
+
+def MultiplyStr(num1, num2):
+    n = 0
+    base = 1
+    
+    while num1>0:
+        n = n + (num1%10) * base
+        base = base * 10
+        num1 = int(num1/10)
+        
+    base = 1
+    m = 0
+    
+    while num2>0:
+        m = m + (num2%10) * base
+        base = base * 10
+        num2 = int(num2/10)
+        
+    return n*m
+        
+
+num1 = 100
+num2 = 150
+
+result = MultiplyStr(num1, num2)
+
+print(result)
